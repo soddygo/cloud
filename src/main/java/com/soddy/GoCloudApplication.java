f73ba6cb8b76e75986ae5c3522f679575a0c28df
soddygo <soddygo@qq.com>
2017-04-27 11:46:53 +0800

    save template demo

---
+++ src/main/java/com/soddy/GoCloudApplication.java
@@ -0,0 +1,12 @@
+package com.soddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoCloudApplication.class, args);
+	}
+}
